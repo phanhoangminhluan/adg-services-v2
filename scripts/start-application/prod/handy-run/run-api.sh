7ee121d8cb9a3a334a6bc150672c6ba6d916c575
minhluan_amanotes <phanhoangminhluan@gmail.com>
2022-05-19 00:54:22 +0700

    add docker file

---
+++ scripts/start-application/prod/handy-run/run-api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+PROJECT_PATH="/Users/luan.phm/engineering/Projects/ADongGroup/adg-services-v2"
+SCRIPT_PATH="./scripts/start-application/dev/customized-run/run.sh"
+
+cd $PROJECT_PATH
+
+echo "Run adg-api service"
+
+sh $SCRIPT_PATH api dev
